c1758a084bfe9c7eb0458217f9fdc8f996925000
carloDM <carlo.de.mauro.work@gmail.com>
2023-03-22 14:46:41 +0100

    complete better

--- js/script.js
+++ js/script.js
@@ -6,28 +6,29 @@ for (let i = 1; i < 101 ; i++ ){
   const box= document.createElement("div");
   // aggiungere la classe al div
   box.classList.add("my_box");
-  // aggiungere i al div
-  box.append(i);
 
   console.log ('div creato', box);
-
+  
 // 2. creare condizioni  
 
-  if (!(i % 3)){
-    console.log('divis x 3  FIZZ', i);
-    box.classList.add("fizz");
-    box.append(' fizz');
-  }
-  if (!(i % 5)){
-    console.log('divis x 5 BUZZ', i);
-    box.classList.add("buzz");
-    box.append(' buzz');
-  }
   if (!(i % 3)&& !(i % 5)){
     console.log('div X 3 && X 5 FIZZBUZZ', i);
-    box.classList.remove("buzz");
-    box.classList.remove("fizz");
     box.classList.add("fizz_buzz");
+    box.append(i, ' FizzBuzz');
+  }
+  else if (!(i % 3)){
+    console.log('divis x 3  FIZZ', i);
+    box.classList.add("fizz");
+    box.append(i, ' fizz');
+
+  }
+  else if (!(i % 5)){
+    console.log('divis x 5 BUZZ', i);
+    box.classList.add("buzz");
+    box.append(i, ' buzz');
+  }
+  else {
+    box.append(i);
   }
   // stampa su html
   document.querySelector(".my_container").append(box);
