d4b92fd04f010df56af413b8964cb4925ff36c5d
carloDM <carlo.de.mauro.work@gmail.com>
2023-03-22 13:27:27 +0100

    divisibile sia per 3 che per 5

---
+++ js/script.js
@@ -0,0 +1,9 @@
+// 1. stampare in console un ciclo for 
+
+for (let i = 0; i < 100 ; i++ ){
+  console.log('i', i)
+  if (!(i % 3)&& !(i % 5)){
+    console.log('divisibile per 3 e per 5', i)
+  }
+
+}
